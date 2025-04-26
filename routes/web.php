2f024ef7074b04e723bb69ac9658aab2ae17891f
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-26 13:23:52 -0300

    CH: Rename index route to /

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\FreteController;
 
+Route::get('/', [FreteController::class, 'index'])->name('frete.index');        // Lista de fretes
 Route::get('/frete', [FreteController::class, 'create'])->name('frete.create');       // Formulário
 Route::post('/frete', [FreteController::class, 'store'])->name('frete.store');        // Processa formulário
-Route::get('/fretes', [FreteController::class, 'index'])->name('frete.index');        // Lista de fretes
 Route::get('/frete/resultado/{id}', [FreteController::class, 'resultado'])->name('frete.resultado'); // Resultado
 
 
