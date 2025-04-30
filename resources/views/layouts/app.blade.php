1f134e20fa2e513b460e5d684a74594bf96bbb56
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-30 20:05:54 -0300

    ADD: Add header to navigate between pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,21 @@
 <!-- vim: set ft=php -->
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-    <title>Frete App</title>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Sistema de Fretes')</title>
+    <!-- <link href="https://cdn.tailwindcss.com" rel="stylesheet"> {{-- Ou seu CSS --}} -->
+    <link href="../../css/header.css" rel="stylesheet">
 </head>
-<body>
-    @yield('content')
+<body class="bg-gray-100 text-gray-800">
+    <div class="container mx-auto mt-8 p-4 bg-white rounded shadow">
+            <header>
+                <a href="{{ route('frete.create') }}" class="text-blue-500 underline">Formulário</a>
+                <a href="{{ route('frete.index') }}" class="text-blue-500 underline">Lista de Fretes</a>
+            </header>
+        @yield('content')
+    </div>
 </body>
 </html>
+
 
