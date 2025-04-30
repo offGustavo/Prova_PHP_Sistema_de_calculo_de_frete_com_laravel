64f32e0d6970eb494c9ec26e8b08d247bbf0b15f
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-30 19:10:12 -0300

    CH: Change route to /fretes para listar todos os fretes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\FreteController;
 
-Route::get('/', [FreteController::class, 'index'])->name('frete.index');        // Lista de fretes
+Route::get('/fretes', [FreteController::class, 'index'])->name('frete.index');        // Lista de fretes
 Route::get('/frete', [FreteController::class, 'create'])->name('frete.create');       // Formulário
 Route::post('/frete', [FreteController::class, 'store'])->name('frete.store');        // Processa formulário
 Route::get('/frete/resultado/{id}', [FreteController::class, 'resultado'])->name('frete.resultado'); // Resultado
