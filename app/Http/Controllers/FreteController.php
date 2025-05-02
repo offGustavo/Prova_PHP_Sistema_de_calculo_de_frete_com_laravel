b407c6ba4366b939541f2234b9f4e24014b0a3b8
Luiz Gustavo <luizgmdcm@gmail.com>
2025-05-02 16:01:43 -0300

    ADD: add metodhs to handle new functions

--- app/Http/Controllers/FreteController.php
+++ app/Http/Controllers/FreteController.php
@@ -4,28 +4,54 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Frete;
+use Barryvdh\DomPDF\Facade\Pdf;
 
-class FreteController
+class FreteController extends Controller
 {
 	/**
 	 * Display a listing of the resource.
 	 */
 
-	public function index()
-	{
-		$fretes = Frete::all();
-		return view('fretes.index', compact('fretes'));
-	}
+	// public function index()
+	// {
+	// 	$fretes = Frete::all();
+	// 	return view('fretes.index', compact('fretes'));
+	// }
 
-	/**
-	 * Show the form for creating a new resource.
-	 */
-	public function create()
-	{
+    public function index(Request $request)
+    {
+        $query = Frete::query();
+
+        if ($request->filled('tipo_frete')) {
+            $query->where('tipo_frete', $request->tipo_frete);
+        }
+
+        if ($request->filled('nome_cliente')) {
+            $query->where('nome_cliente', 'like', '%' . $request->nome_cliente . '%');
+        }
+
+        if ($request->filled('valor_min')) {
+            $query->where('valor_total', '>=', $request->valor_min);
+        }
+
+        if ($request->filled('valor_max')) {
+            $query->where('valor_total', '<=', $request->valor_max);
+        }
+
+        $fretes = $query->paginate(10);
+
+        return view('fretes.index', compact('fretes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
         return view('fretes.create');
     }
 
-	/**
+    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -55,15 +81,16 @@ class FreteController
 
         return view('fretes.resultado', compact('frete'));
     }
-    
-    
+
+
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $frete = Frete::findOrFail($id);
+        return view('frete.show', compact('frete'));
     }
 
     /**
@@ -89,4 +116,26 @@ class FreteController
     {
         //
     }
+
+
+public function exportPdf(Request $request)
+{
+    $query = Frete::query();
+    if ($request->filled('tipo_frete')) {
+        $query->where('tipo_frete', $request->tipo_frete);
+    }
+    if ($request->filled('nome_cliente')) {
+        $query->where('nome_cliente', 'like', '%' . $request->nome_cliente . '%');
+    }
+    if ($request->filled('valor_min')) {
+        $query->where('valor_total', '>=', $request->valor_min);
+    }
+    if ($request->filled('valor_max')) {
+        $query->where('valor_total', '<=', $request->valor_max);
+    }
+    $fretes = $query->get();
+    $pdf = Pdf::loadView('fretes.pdf', compact('fretes'));
+    return $pdf->download('relatorio-fretes.pdf');
+}
+
 }
